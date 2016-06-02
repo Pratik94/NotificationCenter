cbcaf3ddec2b353a5fedca453f36ed8933bf21a5
Pratik Prajapati <pratik.p@servify.in>
2016-06-02 23:11:35 +0530

    RetrofitCall created

---
+++ app/src/main/java/com/crackerjack/notificationcenter/base/RetrofitCallback.java
@@ -0,0 +1,48 @@
+package com.crackerjack.notificationcenter.base;
+
+import android.content.Context;
+
+import com.orhanobut.logger.Logger;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Ashish on 20/04/16.
+ */
+public abstract class RetrofitCallback<T> implements Callback<T> {
+
+    private Context context;
+
+    public RetrofitCallback(Context context) {
+        this.context = context;
+    }
+
+    public RetrofitCallback() {
+    }
+
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+//        Logger.json(new Gson().toJson(response.body()));
+
+        onSuccess(response.body());
+
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+
+//        Utility.getInstance().dismissProgressBar();
+        if (t instanceof IOException) {
+            Logger.v("Failure error " + t.getLocalizedMessage());
+        }
+
+    }
+
+    protected abstract void onSuccess(T response);
+
+
+}
